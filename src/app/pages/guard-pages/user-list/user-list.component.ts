cb55284baf45af13c9248ae522553c5ca6b9400a
Gabriel Gambarra <gabriel.gambarra@dbccompany.com.br>
2022-12-29 23:29:02 -0400

    Create user list; call each edit, delete and show info module

--- src/app/pages/guard-pages/user-list/user-list.component.ts
+++ src/app/pages/guard-pages/user-list/user-list.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { CreateEditModalComponent } from 'src/app/components/create-edit-modal/create-edit-modal.component';
+import { DeleteUserModalComponent } from 'src/app/components/delete-user-modal/delete-user-modal.component';
+import { UserInfoComponent } from 'src/app/components/user-info/user-info.component';
+import { UserListData } from 'src/app/providers/models/UserListResponse.interface';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'lastname', 'actions'];
+  dataSource = new MatTableDataSource<UserListData>([]);
+  page: number = 1;
+  total!: number;
 
+  constructor(private userService: UserService, public dialog: MatDialog) {}
+
+  ngOnInit() {
+    this.getUserList();
+  }
+
+  getUserList(event?: any) {
+    if (event) {
+      this.page = event.pageIndex + 1;
+    }
+    this.userService.getAllUsers(this.page).subscribe((data) => {
+      this.dataSource = new MatTableDataSource<UserListData>(data.data);
+      this.page = data.page;
+      this.total = data.total;
+    });
+  }
+
+  openInfo(element: UserListData): void {
+    this.dialog.open(UserInfoComponent, {
+      data: element.id,
+      maxWidth: '80vw',
+    });
+  }
+
+  openEdit(element: UserListData): void {
+    const editModal = this.dialog.open(CreateEditModalComponent, {
+      data: { element, type: 'edit' },
+    });
+
+    editModal.afterClosed().subscribe((result) => {
+      if (result) {
+        this.userService.updateUser(element.id, result).subscribe((data) => {
+          this.dataSource.data = this.dataSource.data.map((item) =>
+            item.id === element.id ? data : item
+          );
+        });
+      }
+    });
+  }
+
+  openDelete(element: UserListData): void {
+    const deleteModal = this.dialog.open(DeleteUserModalComponent, {
+      data: { ...element },
+    });
+
+    deleteModal.afterClosed().subscribe((result) => {
+      if (result) {
+        this.userService.deleteUserById(element.id).subscribe(() => {
+          this.dataSource.data = this.dataSource.data.filter(
+            (item) => item.id !== element.id
+          );
+        });
+      }
+    });
+  }
 }
--- src/app/pages/guard-pages/user-list/user-list.module.ts
+++ src/app/pages/guard-pages/user-list/user-list.module.ts
@@ -3,9 +3,30 @@ import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list.component';
 import { RouterModule } from '@angular/router';
 import { UserListRoutes } from './user-list.routing';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CreateEditModalModule } from 'src/app/components/create-edit-modal/create-edit-modal.module';
+import { DeleteUserModalModule } from 'src/app/components/delete-user-modal/delete-user-modal.module';
+import { UserInfoModule } from 'src/app/components/user-info/user-info.module';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule, RouterModule.forChild(UserListRoutes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(UserListRoutes),
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatDialogModule,
+    CreateEditModalModule,
+    DeleteUserModalModule,
+    UserInfoModule,
+  ],
 })
 export class UserListModule {}
